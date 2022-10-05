cfa1942c585480f02b0b0969f241cb9d1e6f60b3
sudaksudak <sveta_kiev_ua@yahoo.com>
2022-10-05 15:11:20 +0200

    updated future forecast
    updated future forecast

--- fahrenheit.js
+++ fahrenheit.js
@@ -25,19 +25,25 @@ function formatDate(timestamp) {
 }
 
 
+function formatDay(timestamp){
+let date = new Date (timestamp*1000);
+let day = date.getDay();
+let days = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
+  
+];
 
 
 
+return days[day];
 
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -54,6 +60,7 @@ function displayTemperature(response) {
   let timeElement = document.querySelector("#time");
   let iconElement = document.querySelector("#icon");
 
+  console.log(response.data);
 
 
 
@@ -70,10 +77,13 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+getForecast(response.data.coord);
+
 }
 
 function search(city) {
-  let apiKey = "3499ef150985eccadd080ff408a018df";
+  let apiKey = "96771e971243152d6b8948878c26adde";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -84,7 +94,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("New York");
+search("Berlin");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
@@ -93,28 +103,35 @@ form.addEventListener("submit", handleSubmit);
 //all forecast data
 
 
-function displayForecast(){
 
+function displayForecast(response){
+let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
 let forecastHTML = `<div class="row">`;
-let days = ["Thu", "Fri", "Sat", "Sun"];
-days.forEach(function(day){
+
+forecast.forEach(function(forecastDay, index){
+if (index < 6){
+
+
   forecastHTML = forecastHTML + 
   `  
   <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="http://openweathermap.org/img/wn/50d@2x.png" alt=""
-    width = "42"                  
-    
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+
+
+
+    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"                  
+   
+
     />
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max"> 18°</span>
-      <span class="weather-forecast-temperature-min"> 12°</span>
+      <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+      <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
     </div>
   </div>
   
-`;
+`;}
 
 });
 
@@ -123,5 +140,15 @@ forecastElement.innerHTML= forecastHTML;
 
 }
 
+function getForecast(coordinates){
+console.log(coordinates);
+let apiKey = "96771e971243152d6b8948878c26adde";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displayForecast);
+
+}
+
 //display all forecast
-displayForecast();
+// displayForecast();
